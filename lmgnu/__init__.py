87dadd49c0313c71528caca828b61bc818500912
Eamon <eamon112009@gmail.com>
2026-07-21 22:39:10 +0530

    Initialize lmgnu module and define exports

--- lmgnu/__init__.py
+++ lmgnu/__init__.py
@@ -1,1 +1,9 @@
+# lmgnu/__init__.py
 
+from .logarithm import ln, log10, log2, log_base
+from .core import ScalarNode, Tensor, SequentialNetwork, DenseLayer, LinearUnit
+
+__all__ = [
+    "ln", "log10", "log2", "log_base",
+    "ScalarNode", "Tensor", "SequentialNetwork", "DenseLayer", "LinearUnit"
+]
